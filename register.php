88c9e4b6cb4711ebced1cfdfb5c952299fae09c6
sharclen <nicholashasian@gmail.com>
2023-11-16 21:07:01 +0700

    Register hampir jadi, tinggal dirapihin dikit

--- register.php
+++ register.php
@@ -5,40 +5,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="css/register.css">
+    <link href='https://fonts.googleapis.com/css?family=Source Serif Pro' rel='stylesheet'>
 </head>
 <body>
 
-        <!-- <video autoplay muted loop class="bgvideo">
-            <source src="assets/aot-transform.mp4" type="video/mp4">
-        </video> -->
+<video autoplay muted loop id="bgvideo">
+  <source src="assets/aot-transform.mp4" type="video/mp4">
+</video>
 
 <div class="container">
-        <div class="left">
-            <img class="logo" src="assets/aot-logo.png" alt="logo">
-            <div class="loginform">
-            <h1>Register</h1>
-            <form>
-                <p>Nickname</p> 
-                <input type="text" name="nickname" placeholder="Your Nickname" required>
-                <p>Name</p> 
-                <input type="text" name="name" placeholder="Your Name" required>
-                <p>Password</p>
-                <input type="password" name="password" placeholder="Password" required>
-                <p>Confirm Password</p>
-                <input type="password" name="password" placeholder="Confirm Password" required>
-                <p>Choose Your Race</p>
-                <div class="race-selection">
-                    <img class="eldia" src="assets/logoeldia.png" alt="eldia">
-                    <img class="marley" src="assets/logomarley.png" alt="marley">
+        <img src="assets/aot-logo.png" alt="logo">
+            <h1>The Founder Emerges!</h1>
+            <p>As the Wall Titans unleashed by Eren approach trampling the earth, the survivors can only warn of the apocalyptic force that will soon destroy everything in it’s path.</p>
+            <form action="includes/login.inc.php" method="post">
+                <div class="input-grid">
+                <input type="text" name="nickname" placeholder="YOUR NICKNAME">
+                <input type="password" name="password" placeholder="YOUR PASSWORD">
+                <input type="text" name="name" placeholder="YOUR NAME">
+                <input type="password" name="confirmpassword" placeholder="CONFIRM PASSWORD">
                 </div>
-                <input type="submit" name="register" value="Register">
-                <p>Already have an account ? <a href="login.php" style="color: white; text-decoration: none;"><u>Login</u></a></p>
+            <P>WHAT ARE YOU ?</P>
 
-            </form>
+            <div class="image-grid">
+                <img src="assets/eldian.png" alt="eldian" id="race">
+                <img src="assets/marley.png" alt="marley" id="race">
             </div>
-        </div>
+
+                <input type="submit" name="register" value="CREATE ACCOUNT">
+            <a href="register.php">ALREADY HAVE AN ACCOUNT ?</a>
+
+ </div>
         
-        <div class="right">
+        <!-- <div class="right">
 
         <video autoplay loop id="video">
             <source src="assets/aot-transform.mp4" type="video/mp4">
@@ -51,7 +49,7 @@
 window.addEventListener('load', function() {
     var video = document.getElementById('video');
     video.play();
-});
+}); -->
 
     </script>
 </body>
