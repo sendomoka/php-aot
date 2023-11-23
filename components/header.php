873dc7fb87db95c47c64de9629805ff97791b833
sharclen <nicholashasian@gmail.com>
2023-11-23 10:55:57 +0700

    Added header

--- components/header.php
+++ components/header.php
@@ -3,48 +3,68 @@
 .header {
     display: flex;
     justify-content: space-between;
+    height: 4rem;
+    background-image: url('assets/main-bg.png');
+    background-size: cover;
+    opacity: 0.8;
+    z-index: 999;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    padding: 0 3rem;
+
+}
+
+.left-side{
+    display: flex;
     align-items: center;
-    background-color: white;
+}
+
+.left-side img{
+    height: 100px;
     width: 100%;
-    height: 60px;
-}
-
-.center-logo img {
-    height: 90px;
-    width: 15%; 
-}
-
-.center-logo {
-    flex-grow: 1;
-    text-align: center;
 }
 
 .right-side {
     display: flex;
     align-items: center;
 }
-.right-side span {
-    margin-right: 10px;
-}
+
 .right-side img {
     width: 50px;
     height: 50px;
     border-radius: 50%;
 }
-p{
+
+.user-info {
+    margin-left: 10px;
+}
+
+.user-info .username {
+    font-weight: bold;
     color: white;
+}
+
+.user-info .role {
+    font-size: 0.8em;
+    color: gray;
 }
 
 </style>
 
 <header class="header">
-    <div class="center-logo">
-        <a href="index.php">
+    <div class="left-side">
+    <a href="index.php">
             <img src="assets/aot-logo.png" alt="Logo">
         </a>
     </div>
+    
     <div class="right-side">
-        <p>Welcome, ...</p>
-        <img src="assets/user-photo.png" alt="User Photo">
+    <img src="assets/Vector.png" alt="User Image">
+    <div class="user-info">
+        <p class="username">User Name</p>
+        <p class="role">Marley</p>
     </div>
+</div>
 </header>
