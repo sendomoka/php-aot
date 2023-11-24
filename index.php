f8cea9ec2cf7438e63aa96ce5411c2d7c180de33
sharclen <nicholashasian@gmail.com>
2023-11-25 00:13:13 +0700

    Penambahana script buat section 1 index.php

--- index.php
+++ index.php
@@ -21,16 +21,29 @@
                 <tr>
                     <td>
                         <p>
-                            As the Wall Titans unleashed by Eren approach trampling <br>
-                            the earth, the survivors can only warn of the apocalyptic<br>
-                            force that will soon destroy everything in it’s path.
+                            As the Wall Titans unleashed by Eren approach 
+                            trampling the earth, the survivors can only 
+                            warn of the apocalyptic force that will soon 
+                            destroy everything in it’s path.
                         </p>
                     </td>
                 </tr>
 
                 <tr>
-                    <td>
-                        <img src="assets/ereh1.png" alt="">
+                    <td> 
+                        <div class="hover-container">
+
+                                <img name="ereh" src="assets/ereh1.png" alt="" onmouseover="ganti_gambar2()" onmouseout="ganti_gambar1()">
+                                <div id="click-text" class="click-text">Click the Founder</div>
+                                <audio id="hover-sound" src="assets/erenroar.mp3" preload="auto" muted></audio>
+
+                        <div class="hover-text" id="text">
+                            <a href="register.php" class="button">
+                                START THE END
+                            </a>
+                        </div>
+                        
+                        </div>
                     </td>
                 </tr>
 
@@ -48,17 +61,61 @@
         </div>
 
         <div class="container1bottom">
-            <img src="assets/scrolldown.png" alt="">
+            <table>
+                <tr>
+                    <td><p>Story Begin<p></td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="assets/arrow-down.png" alt="arrow">
+                    </td>
+                </tr>
+            </table>
+            
         </div>
 
     </div>
 
     <div class="container2">
-        <img src="assets/willyspeech.png" alt="">
+        
+
         <div class="speech">
-            
+            <table>
+
+                <tr>
+                    <td>
+                        <img src="assets/speech.png" alt="">
+                    </td>
+                </tr>
+
+            </table>
         </div>
+
+        <div class="speechtext">
+            <table>
+                <tr>
+                    <td>
+                        <p>
+                        While we saw the rise of weapons more powerful than Titans in the last war... 
+                        we will never see a weapon that can stop the advance of millions of Colossal Titans. 
+                        If the Walls walk even a single time... there will be nothing left we can do. 
+                        All that can remain for humanity would be to flee in terror at the rumble of 
+                        the footsteps that signal our end. These massive creatures would trample our 
+                        cities, our societies... They would crush the flora and fauna in every ecosystem. 
+                        They would literally flatten our world.
+                        </p>
+                        <small>— Willy Tybur explains the destructive potential of the Rumbling to his audience during the Liberio festival</small>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="container2bottom">
+            <img src="assets/scrolldown.png" alt="scrollimg">
+        </div>
+
     </div>
 
+    <script src="js/index.js"> </script>
 </body>
 </html>
