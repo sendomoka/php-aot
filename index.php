fce6489caa4373f9be873b91a704d271cd5451aa
sharclen <nicholashasian@gmail.com>
2023-11-26 00:16:38 +0700

    section 2 kelar

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
 
     <div class="container2">  
 
-        <div class="upperimage">
+        <div class="upperimage" id="upperimage">
             <table>
                 <tr>
                     <td>
@@ -85,7 +85,32 @@
             </table>
         </div>
 
-        <div class="speechtext">
+        <button id="playButton"></button>
+
+        <div class="speechfull" id="speechfull">
+            <table>
+                <tr>
+                    <td>
+                            <span class="speech-part"> Karl Fritz used the power of the Founding Titan to build three concentric walls. </span> 
+                            <span class="speech-part"> These walls, made of untold millions of Collosals Titans, </span> 
+                            <span class="speech-part"> have acted as both spear and shield to protect the world's peace until now. </span> 
+                            <span class="speech-part"> However, several years ago, a revolution occured on Paradis Island. </span> 
+                            <span class="speech-part"> King Fritz's peaceful ideology has been overthrown, and the Founding Titan has been stolen by a certain individual. </span> 
+                            <span class="speech-part"> The world is once again in danger. </span> 
+                            <span class="speech-part"> The name of this rebel who threatens our peace is Eren Yeager </span> 
+                            <span class="speech-part"> The threat posed by Paradis Island is the attack of this army of Collosal Titans: The Rumbling. </span> 
+                            <span class="speech-part"> As i explained earlier, the royal bloodline is bound by the pledge renouncing war and cannot utilize the power of the Founding Titan. </span> 
+                            <span class="speech-part"> but Eren Yeager, who now holds the Founding Titan is not bound by any such promise. </span> 
+                            <span class="speech-part"> If he begins the Rumbling, there will be nothing we can do. </span> 
+                            <span class="speech-part"> Humanity will be powerless but to cower in fear of the footfalls of the apocalypse. </span> 
+                            <span class="speech-part"> The civilization and cities of our world will be destroyed </span> 
+                            <span class="speech-part"> as everything we know and love is literally crushed flat. </span>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+        <div class="speechshort" id="speechshort">
             <table>
                 <tr>
                     <td>
@@ -102,9 +127,23 @@
             </table>
         </div>
 
+        <!-- <div class="container2bottom">
+            <table>
+                <tr>
+                    <td><p>Story Begin<p></td>
+                </tr>
+                <tr>
+                    <td>
+                        <img src="assets/arrow-down.png" alt="arrow">
+                    </td>
+                </tr>
+            </table>   
+            </div> -->
+
         <div class="fuzzy-overlay"></div>
     </div>
 
+    <audio id="sound" src="assets/willyspeech.mp3" preload="auto"></audio>
     <script src="js/index.js"> </script>
 </body>
 </html>
--- js/index.js
+++ js/index.js
@@ -19,3 +19,33 @@ document.querySelector('img[name="ereh"]').addEventListener('click', function() 
 document.querySelector('img[name="ereh"]').addEventListener('click', function() {
     document.getElementById('click-text').style.display = 'none';
 });
+
+document.getElementById('playButton').addEventListener('click', function() {
+    this.style.display = 'none';
+    document.getElementById('sound').play();
+});
+
+document.getElementById('playButton').addEventListener('click', function() {
+    document.getElementById('upperimage').classList.add('fadeOut');
+    this.style.display = 'none';
+    document.getElementById('sound').play();
+    document.getElementById('speechfull').style.display = 'block';
+    document.getElementById('speechshort').style.display = 'none';
+
+    var delays = [5000, 6880, 5000, 4630, 4870, 7000, 4140, 6750, 7800, 7950, 9060, 5840, 5640,];
+    var parts = document.getElementsByClassName('speech-part');
+    var cumulativeDelay = 0;
+    for (let i = 0; i < parts.length; i++) {
+        cumulativeDelay += delays[i % delays.length];
+        setTimeout(function() {
+            parts[i].style.display = 'block';
+        }, cumulativeDelay);
+    }
+});
+
+document.getElementById('sound').addEventListener('ended', function() {
+    document.getElementById('upperimage').classList.remove('fadeOut');
+    document.getElementById('upperimage').classList.add('fadeIn');
+    document.getElementById('speechfull').style.display = 'none';
+    document.getElementById('speechshort').style.display = 'block';
+});
