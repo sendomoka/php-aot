a374eeeea9154798b73e4fd1c2f389c908b8ff93
sharclen <nicholashasian@gmail.com>
2023-11-29 09:05:04 +0700

    Gatau lupa ganti apa aja

--- dashboard.php
+++ dashboard.php
@@ -13,14 +13,16 @@
         <a href="index.php">    
         <img src="assets/aot-logo.png" alt="aot">
         </a>
-        <button id="btn1" class="active">Setting</button>
-        <button id="btn2">Timeline</button>
-        <button id="btn3">Death</button>
-        <button id="btn4">User</button>
+        <button id="btn1" class="active"> <img src="assets/settingicon.png" alt="icon"> Setting</button>
+        <button id="btn2"> <img src="assets/tlicon.png" alt="icon"> Timeline</button>
+        <button id="btn3"> <img src="assets/deathicon.png" alt="icon"> Death</button>
+        <button id="btn4"> <img src="assets/usericon.png" alt="icon"> User</button>
+
+        <button id="logout"><img src="assets/logouticon.png" alt="icon">Logout</button>
     </div>
 
     <div class="atas">
-        <h1>Setting</h1>
+        <h1 id="header">Setting</h1>
         <img src="assets/Vector.png" alt="">
         <p class="username">Jehian Athaya</p>
         <p class="role">Eldian</p>
@@ -36,7 +38,8 @@
                 </div>
 
                 <div class="input-group-2">
-                    <input type="file">
+                    <label for="avatarUpload" class="file-upload-button">Choose Image</label>
+                    <input type="file" id="avatarUpload" class="avatar-upload" accept=".png, .jpg, .jpeg" style="display: none;">
                 </div>
 
                 <div class="input-group-nick">
@@ -54,33 +57,66 @@
                     <input type="password" name="password" value="12345">
                 </div>
 
-                <input type="submit" value="Confirm">
+                <input type="submit" value="Edit Data">
             </div>
         </form>
     </div>
 
     <div class="content2" id="content-2">
-        <form method="post">
-            <div class="form-group">
+        <button id="leftbutton"> <img src="assets/addicon.png" alt="icon"> Add Data</button>
+        <input type="search" id="searchBar" placeholder="Search...">
 
-                <div class="input-group-nick">
-                    <p>Nickname</p>
-                    <input type="text" name="nickname" value="jehath">
-                </div>
-                
-                <div class="input-group-name">
-                    <p>Name</p>
-                    <input type="text" name="name" value="Jehian Athaya">
-                </div>
+        <table id="contentTable">
 
-                <div class="input-group-pw">
-                    <p>Password</p>
-                    <input type="password" name="password" value="12345">
-                </div>
+            <tr>
+                <th>No</th>
+                <th>Place</th>
+                <th>Time</th>
+                <th>Action</th>
+            </tr>
 
-                <input type="submit" value="Confirm">
+            <tr>
+                <td>1.</td>
+                <td>Mitras, Paradise</td>
+                <td>Day 1</td>
+                <td>
+                    <img src="assets/detailicon.png" alt="icon" class="action-img">
+                    <img src="assets/editicon.png" alt="icon" class="action-img">
+                    <img src="assets/deleteicon.png" alt="icon" class="action-img">
+                </td>
+            </tr>
+
+        </table>
+    </div>
+
+    <div class="content2-1" id="content-2-1">
+    <form method="post">
+        <div class="form-group">
+
+            <div class="input-group-place">
+                <p>Place</p>
+                <input type="text" name="place" value="Mitras, Paradise">
             </div>
-        </form>
+            
+            <div class="input-group-detail">
+                <p>Detail</p>
+                <textarea name="detail" id="detail" cols="30" rows="7">Mitras is the opulent capital city and center of power within Wall Sina, where nobles and aristocrats enjoy extravagant wealth detached from commoners struggling daily against the Titan threat outside.</textarea>
+            </div>
+
+            <div class="input-group-time">
+                <p>Time</p>
+                <input type="text" name="time" value="Day 1">
+            </div>
+
+            <div class="input-group-image">
+                <p>Image</p>
+                <label for="timelineUpload" class="timeline-upload-button">Choose Image</label>
+                <input type="file" id="timelineUpload" class="timeline-upload" name="timelineimg" accept=".png, .jpg, .jpeg" style="display: none;">
+            </div>
+
+            <input type="submit" value="Add Data">
+        </div>
+    </form>
     </div>
 
     <div class="content3" id="content-3">
--- js/dashboard.js
+++ js/dashboard.js
@@ -18,5 +18,44 @@ buttons.forEach(function(button) {
     });
 });
 
+// Jangan lupa ini diganti kalo mau ngedit content yang lain
+
 buttons[0].classList.add('active');
 document.getElementById('content-1').style.display = 'block';
+
+// Jangan lupa ini diganti
+
+var button1 = document.getElementById('btn1');
+var button2 = document.getElementById('btn2');
+var button3 = document.getElementById('btn3');
+var button4 = document.getElementById('btn4');
+var headerText = document.getElementById('header');
+
+button1.addEventListener('click', function() {
+    headerText.textContent = 'Setting';
+});
+
+button2.addEventListener('click', function() {
+    headerText.textContent = 'Timeline';
+});
+
+button3.addEventListener('click', function() {
+    headerText.textContent = 'Death';
+});
+
+button4.addEventListener('click', function() {
+    headerText.textContent = 'User';
+});
+
+// Tambahin kalo mau nambah content baru
+var leftButton = document.getElementById('leftbutton');
+var content2 = document.getElementById('content-2');
+var content2_1 = document.getElementById('content-2-1');
+
+// Add a click event listener to the left button
+leftButton.addEventListener('click', function() {
+    // Hide content2 and show content2-1
+    content2.style.display = 'none';
+    content2_1.style.display = 'block';
+    headerText.textContent = 'Timeline - Add Data';
+});
