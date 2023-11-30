28bc00b70ccff7450627ce5d5a772e597de8ec18
sharclen <nicholashasian@gmail.com>
2023-11-30 21:42:09 +0700

    Base timeline jadi

---
+++ js/timeline.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+    var containers = document.querySelectorAll('.container');
+    for (var i = 0; i < containers.length; i++) {
+        var circle = document.createElement('div');
+        circle.className = 'circle';
+        containers[i].insertBefore(circle, containers[i].firstChild);
+
+        if (i !== containers.length - 1) {
+            var line = document.createElement('div');
+            line.className = 'line';
+            containers[i].appendChild(line);
+        }
+    }
+}
--- timeline.php
+++ timeline.php
@@ -12,7 +12,8 @@ if (!isset($_SESSION['nickname'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home</title>
+    <title>Timeline</title>
+    <link href='https://fonts.googleapis.com/css?family=Source Serif Pro' rel='stylesheet'>
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/timeline.css">
 </head>
@@ -21,7 +22,34 @@ if (!isset($_SESSION['nickname'])) {
     
         <div class="title">
             <h1>Rumbling Timeline</h1>
-            <h2>Year : 854</h2>
+            <h3>Year : 854</h3>
+        </div>
+
+        <div class="container">
+            <div class="text-content">
+                <h1>Mitras, Paradis</h1>
+                <p>Mitras is the opulent capital city and center of power within Wall Sina, where nobles and aristocrats enjoy extravagant wealth detached from commoners struggling daily against the Titan threat outside.</p>
+                <p><span class="time">Time : Day 1</span>  <span class="death">Death : 83</span></p>
+            </div>
+            <img src="assets/images/day1.png" alt="Your Image">
+        </div>
+
+        <div class="container">
+            <div class="text-content">
+                <h1>Mitras, Paradis</h1>
+                <p>Mitras is the opulent capital city and center of power within Wall Sina, where nobles and aristocrats enjoy extravagant wealth detached from commoners struggling daily against the Titan threat outside.</p>
+                <p><span class="time">Time : Day 1</span>  <span class="death">Death : 83</span></p>
+            </div>
+            <img src="assets/images/day1.png" alt="Your Image">
+        </div>
+
+        <div class="container">
+            <div class="text-content">
+                <h1>Mitras, Paradis</h1>
+                <p>Mitras is the opulent capital city and center of power within Wall Sina, where nobles and aristocrats enjoy extravagant wealth detached from commoners struggling daily against the Titan threat outside.</p>
+                <p><span class="time">Time : Day 1</span>  <span class="death">Death : 83</span></p>
+            </div>
+            <img src="assets/images/day1.png" alt="Your Image">
         </div>
 
     <script src="js/timeline.js"> </script>
