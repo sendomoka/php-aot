c658f5f97950b4a8131ab96b6f095f7c71697845
sharclen <nicholashasian@gmail.com>
2023-11-30 21:53:16 +0700

    Timeline jadi (?)

--- js/timeline.js
+++ js/timeline.js
@@ -2,7 +2,11 @@ window.onload = function() {
     var containers = document.querySelectorAll('.container');
     for (var i = 0; i < containers.length; i++) {
         var circle = document.createElement('div');
-        circle.className = 'circle';
+        if (i !== containers.length - 1) {
+            circle.className = 'circle';
+        } else {
+            circle.className = 'circle-img';
+        }
         containers[i].insertBefore(circle, containers[i].firstChild);
 
         if (i !== containers.length - 1) {
